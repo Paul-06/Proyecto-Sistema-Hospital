d4e8b54bf76aba61c0384766a07eb4164eb2c8a5
Gustavo <gustavo.alvarez.taquia@gmail.com>
2024-05-02 17:01:37 -0500

    vistas de TipoEmpleado, Especialidad

--- SistemaHospital/Controllers/EspecialidadController.cs
+++ SistemaHospital/Controllers/EspecialidadController.cs
@@ -1,18 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaHospital.Models;
 
 namespace SistemaHospital.Controllers
 {
     public class EspecialidadController : Controller
     {
+        private readonly BdHospitalContext _context;
+        public EspecialidadController(BdHospitalContext context)
+        {
+            _context = context;
+        }
+
         #region NAVEGACION
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Upsert()
+        public IActionResult Upsert(int? id)
         {
-            return View();
+            var especialidad = new Especialidad();
+
+            if (id is null)
+            {
+                // Crear nuevo cargo
+                return View(especialidad);
+            }
+
+            // Actualizar cargo
+            especialidad = _context.Especialidads.Find(id.GetValueOrDefault()); // GetValueOrDefault() nos permite trabajar con un valor nulo (ver el parámetro)
+
+            if (especialidad is null)
+            {
+                return NotFound();
+            }
+
+            return View(especialidad);
         }
         #endregion
 
--- SistemaHospital/Controllers/TipoEmpleadoController.cs
+++ SistemaHospital/Controllers/TipoEmpleadoController.cs
@@ -1,18 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaHospital.Models;
 
 namespace SistemaHospital.Controllers
 {
     public class TipoEmpleadoController : Controller
     {
+        private readonly BdHospitalContext _context;
+
+        public TipoEmpleadoController(BdHospitalContext context)
+        {
+            _context = context;
+        }
         #region NAVEGACION
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Upsert()
+        public IActionResult Upsert(int? id)
         {
-            return View();
+            var TipoEmpleado = new TipoEmpleado();
+
+            if (id is null)
+            {
+                // Crear nuevo cargo
+                return View(TipoEmpleado);
+            }
+
+            // Actualizar cargo
+            TipoEmpleado = _context.TipoEmpleados.Find(id.GetValueOrDefault()); // GetValueOrDefault() nos permite trabajar con un valor nulo (ver el parámetro)
+
+            if (TipoEmpleado is null)
+            {
+                return NotFound();
+            }
+
+            return View(TipoEmpleado);
         }
         #endregion
 
