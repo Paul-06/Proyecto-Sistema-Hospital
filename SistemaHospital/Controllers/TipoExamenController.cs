45c7152add2176e5854d25a6078f08ded6cb5c1d
FlorMamani19 <129968938+FlorMamani19@users.noreply.github.com>
2024-05-02 18:57:49 -0500

    Tipo tratamiento y tipo de examen

--- SistemaHospital/Controllers/TipoExamenController.cs
+++ SistemaHospital/Controllers/TipoExamenController.cs
@@ -1,18 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaHospital.Models;
 
 namespace SistemaHospital.Controllers
 {
     public class TipoExamenController : Controller
     {
+        private readonly BdHospitalContext _context;
+        public TipoExamenController(BdHospitalContext context)
+        {
+            _context = context;
+        }
+
         #region NAVEGACION
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Upsert()
+        public IActionResult Upsert(int? id)
         {
-            return View();
+            var tipoExaman = new TipoExaman();
+
+            if (id is null)
+            {
+                // Crear nuevo cargo
+                return View(tipoExaman);
+            }
+
+            // Actualizar cargo
+            tipoExaman = _context.TipoExamen.Find(id.GetValueOrDefault()); // GetValueOrDefault() nos permite trabajar con un valor nulo (ver el parámetro)
+
+            if (tipoExaman is null)
+            {
+                return NotFound();
+            }
+
+            return View(tipoExaman);
         }
         #endregion
 
--- SistemaHospital/Controllers/TipoTratamientoController.cs
+++ SistemaHospital/Controllers/TipoTratamientoController.cs
@@ -1,18 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SistemaHospital.Models;
 
 namespace SistemaHospital.Controllers
 {
     public class TipoTratamientoController : Controller
     {
+        private readonly BdHospitalContext _context;
+        public TipoTratamientoController(BdHospitalContext context)
+        {
+            _context = context;
+        }
         #region NAVEGACION
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Upsert()
+        public IActionResult Upsert(int? id)
         {
-            return View();
+            var tipotratamiento = new TipoTratamiento();
+
+            if (id is null)
+            {
+                // Crear nuevo cargo
+                return View(tipotratamiento);
+            }
+
+            // Actualizar cargo
+            tipotratamiento = _context.TipoTratamientos.Find(id.GetValueOrDefault()); // GetValueOrDefault() nos permite trabajar con un valor nulo (ver el parámetro)
+
+            if (tipotratamiento is null)
+            {
+                return NotFound();
+            }
+
+            return View(tipotratamiento);
         }
         #endregion
 
